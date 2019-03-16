8704ff49fff345f4c0f08991a77be8132c1f8403
Muhammad Aizuddin Zali <mzali@mzali-fedora.redhat.com>
2019-03-16 23:49:21 +0800

    add gitignore and update create_applier script

--- create_applier.sh
+++ create_applier.sh
@@ -11,10 +11,18 @@ if [ "$#" -ne 1 ]; then
 	usage
 fi
 
+echo "Creating $1 directory..."
 mkdir $1 
+
+echo "Creating skeleton directories and files..."
 mkdir -p $1/inventory/group_vars params/{ruby,projectrequests} $1/templates/{app,project}
-touch $1/inventory/group_vars/all.yml $1/inventory/hosts 
+touch $1/inventory/group_vars/all.yml $1/inventory/hosts
+
+echo "Copying openshift-applier installed roles..."
 cp -rf roles $1/roles
+
+echo "Copying apply.yml as common entrypoing..."
+cp apply.yml $1/apply.yml
 
 if [ $? == 0 ]; then
 	echo "$1 skeleton created!"
